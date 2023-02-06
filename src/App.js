9b7ca83985a1194fe8212484bcda3fa46ffe4c39
vitamin777 <rlaalsdn8@gmail.com>
2023-02-06 23:58:29 +0900

    feat: React.lazy&Suspense 사용해 컴포넌트 코드 스플리팅 테스트 중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import notify from "./notify";
+import {Suspense, useState} from "react";
 
+const SplitMe = React.lazy(() => import('./SplitMe'))
 function App() {
+  const [visible, setVisible] = useState(false)
+  const onClick = () => {
+    setVisible(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <heder className='App_header'>
+        <img src={logo} className='App_logo' alt='logo' />
+        <p onClick={onClick}>hello react</p>
+        <Suspense fallback={<div>loading</div>}>
+          {visible && <SplitMe/>}
+        </Suspense>
+      </heder>
     </div>
   );
 }
