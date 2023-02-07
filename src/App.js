4e8213512e5e3c947ed779c262a81a53d1240b74
vitamin777 <rlaalsdn8@gmail.com>
2023-02-07 23:59:00 +0900

    feat: 클릭시 SplitMe component 출력 및 로딩 ui 출력 기능 추가
    -App.js 에서 visible 로 보여질 state 설정 후 Hello React 문구를 클릭하면 onClick 함수에서 setVisible true 설정해 jsx 에서 visible 이 참일 경우 SplitMe 컴포넌트가 보여지도록하였음
    -App.js 에서 loadable 을 사용해 hello react 마우스 오버 시 onMouseOver 함수에 preload 로 로딩이 시작되도록하고 클릭됏을때 렌더링되도록함
    -App.js 에서 SplitMe 에 fallback 으로 로딩 ui 보여주기위해 fallback 으로 loading 작성

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import notify from "./notify";
 import {Suspense, useState} from "react";
+import loadable from "@loadable/component";
 
-const SplitMe = React.lazy(() => import('./SplitMe'))
+// 로딩중 ui 를 보여주기위해 fallback 사용
+const SplitMe = loadable(()=> import('./SplitMe'),{
+  fallback: <div>loading...</div>
+})
+/*const SplitMe = React.lazy(() => import('./SplitMe'))*/
 function App() {
   const [visible, setVisible] = useState(false)
   const onClick = () => {
     setVisible(true)
   }
 
+  // 미리불러오는 preload 방법 마우스 오버시 로딩이 시작되고 클릭했을때 렌더링됨
+  const onMouseOver = () => {
+    SplitMe.preload()
+  }
+
   return (
     <div>
       <heder className='App_header'>
         <img src={logo} className='App_logo' alt='logo' />
-        <p onClick={onClick}>hello react</p>
+        <p onClick={onClick} onMouseOver={onMouseOver}>hello react</p>
         <Suspense fallback={<div>loading</div>}>
           {visible && <SplitMe/>}
         </Suspense>
